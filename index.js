caee801f8e36be869cce7c2df6c722430ba3dc2c
Zander Huff <huffzander@gmail.com>
2020-01-23 20:28:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === shout) {
+  string == lowercase {
     return "I can\'t hear you!"
   }
-  else if (string === whisper) {
+  string == uppercase {
     return "YES INDEED!"
   }
-  else if (string == "I love you, Grandma.") {
+  string == ("I love you, Grandma.") {
     return "I love you, too."
   }
 }
